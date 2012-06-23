81a89503f41b0459da40b169639149bf0bcaa4f5
Jason Sankey <jason@zutubi.com>
2012-06-23 17:52:08 +1000

    Added tests for NdkBuildTask.

--- src/com/zutubi/android/ant/NdkBuildTask.java
+++ src/com/zutubi/android/ant/NdkBuildTask.java
@@ -16,14 +16,31 @@ public class NdkBuildTask extends ExecTask {
      * Name of the Android Ant build property that points to the NDK install
      * directory.
      */
-    private static final String PROPERTY_NDK_DIR = "ndk.dir";
+    public static final String PROPERTY_NDK_DIR = "ndk.dir";
+    /**
+     * Usual ndk-build command.
+     */
+    public static final String NDK_BUILD = "ndk-build";
+    /**
+     * Custom ndk-build command for Windows OS.
+     */
+    public static final String NDK_BUILD_WINDOWS = "ndk-build.cmd";
     /**
      * Name of the ndk-build command, which depends on the OS.
      */
-    private static final String COMMAND_NDK_BUILD = Os.isFamily(Os.FAMILY_WINDOWS) ? "ndk-build.cmd"
-            : "ndk-build";
+    public static final String COMMAND_NDK_BUILD = Os.isFamily(Os.FAMILY_WINDOWS) ? NDK_BUILD_WINDOWS
+            : NDK_BUILD;
 
-    protected String getNdkCommand() {
+    /**
+     * Determines the ndk-build command.  Looks for a platform-specific file
+     * within a directory defined by ndk.build.  If found, the absolute path to
+     * the file is returned.  If not found the platform-specific name is
+     * returned, and the NDK directory must be on the path for this task to
+     * work.
+     *
+     * @return the most specific ndk-build command that can be found
+     */
+    String getNdkBuildCommand() {
         String ndkDir = getProject().getProperty(PROPERTY_NDK_DIR);
         if (Util.stringSet(ndkDir)) {
             File ndkBuild = new File(ndkDir, COMMAND_NDK_BUILD);
@@ -38,7 +55,7 @@ public class NdkBuildTask extends ExecTask {
     @Override
     public void execute() throws BuildException {
         if (!Util.stringSet(cmdl.getExecutable())) {
-            setExecutable(getNdkCommand());
+            setExecutable(getNdkBuildCommand());
         }
 
         super.execute();
---
+++ test/com/zutubi/android/ant/NdkBuildTaskTest.java
@@ -0,0 +1,62 @@
+package com.zutubi.android.ant;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.condition.Os;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class NdkBuildTaskTest {
+    private NdkBuildTask mNdkBuildTask;
+
+    private File mFakeNdkDir;
+    private File mFakeBuildScript;
+    private File mFakeBuildCmd;
+
+    @Before
+    public void setUp() throws IOException {
+        mNdkBuildTask = new NdkBuildTask();
+        mNdkBuildTask.setProject(new Project());
+
+        mFakeNdkDir = File.createTempFile(getClass().getName(), ".tmp");
+        assertTrue(mFakeNdkDir.delete());
+        assertTrue(mFakeNdkDir.mkdir());
+
+        mFakeBuildScript = new File(mFakeNdkDir, "ndk-build");
+        assertTrue(mFakeBuildScript.createNewFile());
+
+        mFakeBuildCmd = new File(mFakeNdkDir, "ndk-build.cmd");
+        assertTrue(mFakeBuildCmd.createNewFile());
+    }
+
+    @After
+    public void tearDown() {
+        assertTrue(mFakeBuildCmd.delete());
+        assertTrue(mFakeBuildScript.delete());
+        assertTrue(mFakeNdkDir.delete());
+    }
+
+    @Test
+    public void testGetNdkBuildCommandNoNdkDirProperty() {
+        assertEquals(NdkBuildTask.COMMAND_NDK_BUILD, mNdkBuildTask.getNdkBuildCommand());
+    }
+
+    @Test
+    public void testGetNdkBuildCommandBadNdkDirProperty() {
+        mNdkBuildTask.getProject().setProperty(NdkBuildTask.PROPERTY_NDK_DIR, "/no/such/dir");
+        assertEquals(NdkBuildTask.COMMAND_NDK_BUILD, mNdkBuildTask.getNdkBuildCommand());
+    }
+
+    @Test
+    public void testGetNdkBuildCommandNdkDirProperty() {
+        mNdkBuildTask.getProject().setProperty(NdkBuildTask.PROPERTY_NDK_DIR, mFakeNdkDir.getAbsolutePath());
+        File expectedCommand = Os.isFamily(Os.FAMILY_WINDOWS) ? mFakeBuildCmd : mFakeBuildScript;
+        assertEquals(expectedCommand.getAbsolutePath(), mNdkBuildTask.getNdkBuildCommand());
+    }
+}
