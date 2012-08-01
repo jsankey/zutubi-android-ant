282b6cde00df1219d7b3789a0f0d83f75b4ae8ba
Jason Sankey <jason@zutubi.com>
2012-08-01 23:09:32 +1000

    Added a libproperties task which can create the properties files used by ADT tools to link libraries to their sources in Eclipse.  No tests as yet but an example to show it works.

---
+++ src/com/zutubi/android/ant/LibraryPropertiesTask.java
@@ -0,0 +1,109 @@
+package com.zutubi.android.ant;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.util.FileUtils;
+
+/**
+ * A task to generate .properties files alongside jars, pointing to the
+ * corresponding source jars where available.  This allows the ADT tools to
+ * link the sources to the libraries in Eclipse.
+ */
+public class LibraryPropertiesTask extends Task {
+    private String libsDir = "libs";
+    private String jarPattern = "(.+)\\.jar";
+    private String srcReplacement = "src/$1-sources.jar";
+
+    /**
+     * Sets the path of the directory where library jars are found.
+     *
+     * @param libsDir path, relative to the build base, of the directory
+     *                including the library jars
+     */
+    public void setLibsDir(final String libsDir) {
+        this.libsDir = libsDir;
+    }
+
+    /**
+     * Sets the regular expression used to match jar files.  Any file within
+     * libsDir with a name matching this pattern will be processed as a jar.
+     * Typically the pattern includes capturing groups referenced in the
+     * srcReplacement.
+     *
+     * @param jarPattern regular expression used to identify jar files and
+     *                   capture name components for later use
+     */
+    public void setJarPattern(final String jarPattern) {
+        this.jarPattern = jarPattern;
+    }
+
+    /**
+     * Sets the replacement string used to generate the path of a source jar
+     * from the name of the corresponding library jar.  Typically includes
+     * references to groups captured by the jarPattern.
+     *
+     * @param srcReplacement replacement string used to generate the path of a
+     *                       source jar based on the matched library jar name
+     */
+    public void setSrcReplacement(final String srcReplacement) {
+        this.srcReplacement = srcReplacement;
+    }
+
+    @Override
+    public void execute() throws BuildException {
+        File libs = new File(getProject().getBaseDir(), libsDir);
+        if (!libs.isDirectory()) {
+            throw new BuildException("Libs directory '" + libsDir + "' does not exist");
+        }
+
+        final Pattern jarRegex = Pattern.compile(jarPattern);
+        for (final File file : libs.listFiles()) {
+            if (file.isFile()) {
+                final Matcher matcher = jarRegex.matcher(file.getName());
+                if (matcher.matches()) {
+                    final String srcPath = matcher.replaceFirst(srcReplacement);
+                    final File srcFile = new File(libs, srcPath);
+                    if (srcFile.isFile()) {
+                        createPropertiesLinking(file, srcPath);
+                    }
+                }
+            }
+        }
+    }
+
+    private void createPropertiesLinking(final File jar, final String srcJarPath) {
+        final File propertiesFile = new File(jar.getAbsolutePath() + ".properties");
+        final java.util.Properties properties = new Properties();
+        if (propertiesFile.isFile()) {
+            FileReader reader = null;
+            try {
+                reader = new FileReader(propertiesFile);
+                properties.load(reader);
+            } catch (IOException e) {
+                throw new BuildException("Could not read existing properties", e);
+            } finally {
+                FileUtils.close(reader);
+            }
+        }
+
+        properties.put("src", srcJarPath);
+
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(propertiesFile);
+            properties.store(writer, null);
+        } catch (final IOException e) {
+            throw new BuildException("Could not write out properties", e);
+        } finally {
+            FileUtils.close(writer);
+        }
+    }
+}
